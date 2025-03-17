3467859ec430028761bf1db667d6996e226d5b8b
Teale Shaw <tshaw3@parrastu.catholic.edu.au>
2025-03-18 09:56:29 +1100

    feat(users): add GitHub OAuth (Week 3)
    This commit adds support for user authentication with GitHub via OAuth.

--- pwa_project/urls.py
+++ pwa_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('users/', include(("users.urls", "users"), namespace="users")),
     path('tasks/', include(("tasks.urls", "tasks"), namespace="tasks")),
+    path('accounts/', include('allauth.urls')),
 ]
