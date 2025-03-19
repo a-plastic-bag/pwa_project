79681af5e9117c4f14b4810630b35b2c7efee290
Teale Shaw <tshaw3@parrastu.catholic.edu.au>
2025-03-20 10:16:35 +1100

    feat: add PWA functionality (Week 5)
    Adds preliminary PWA functionality to the project.

--- pwa_project/urls.py
+++ pwa_project/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from tasks import views as task_views
+from .views import serve_service_worker
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('users/', include(("users.urls", "users"), namespace="users")),
     path('tasks/', include(("tasks.urls", "tasks"), namespace="tasks")),
     path('accounts/', include('allauth.urls')),
+    path('', task_views.index, name='home'),
+    path('service-worker.js', serve_service_worker, name='service_worker'),
 ]
