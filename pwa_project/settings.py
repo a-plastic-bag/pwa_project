a-plastic-bag/pwa_project

commit 524a32e198f8e7af3ed781922f7695a336e56a11

refactor: use context processors (Week 7)
Uses a context processor for the username. This allows the variable to
be referred to directly i.e. {{ username }} rather than calling it
manually i.e. {{ request.user.username }}.

pwa_project/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "tasks.context_processors.username",
             ],
         },
     },

tasks/templates/tasks/layout.html
@@ -11,7 +11,7 @@
     <header>
         <div class="user-info">
             {% if request.user.is_authenticated %}
-                <span>{{ request.user.username }}</span>
+                <span>{{ username }}</span>
                 <form action="{% url 'users:logout' %}" method="post">
                     {% csrf_token %}
                     <button type="submit">Logout</button>

users/context_processors.py
@@ -1,0 +1,4 @@
+def username(request):
+    if request.user.is_authenticated:
+        return {'username': request.user.username}
+    return {'username': None}

commit 04b1dc0bda3c317b6d6b0aee7844f6ea37691b61

fix: correct context processor path
Fixes bug introduced in commit 524a32e1: replaces "tasks" with "users"
when calling the "username" context processor.

pwa_project/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "tasks.context_processors.username",
+                "users.context_processors.username",
             ],
         },
     },